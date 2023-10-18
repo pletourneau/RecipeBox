d27f7d569e2667e0a9ea785b4bab1610559e93b1
Jake Elsberry <jelsberry96@gmail.com>
2023-10-18 09:33:18 -0700

    splash page only displays user recipes
    Co-authored-by: Jon Cheng <joncheng.dev@gmail.com>
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- RecipeBox/Controllers/RecipesController.cs
+++ RecipeBox/Controllers/RecipesController.cs
@@ -11,72 +11,74 @@ using System.Security.Claims;
 
 namespace RecipeBox.Controllers
 {
-  // [Authorize]
+  [Authorize]
   public class RecipesController : Controller
   {
     private readonly RecipeBoxContext _db;
-    // private readonly UserManager<ApplicationUser> _userManager;
-    // public RecipesController(UserManager<ApplicationUser> userManager, RecipeBoxContext db)
-    public RecipesController(RecipeBoxContext db)
+    private readonly UserManager<ApplicationUser> _userManager;
+    public RecipesController(UserManager<ApplicationUser> userManager, RecipeBoxContext db)
+    // public RecipesController(RecipeBoxContext db)
     {
-      // _userManager = userManager;
+      _userManager = userManager;
       _db = db;
     }
 
-    public ActionResult Index()
-    { 
+    // public ActionResult Index()
+    // { 
+    //   ViewBag.PageTitle = "List of Recipes";
+    //   List<Recipe> model = _db.Recipes.ToList();
+    //   return View(model);
+    // }
+
+    public async Task<ActionResult> Index()
+    {
       ViewBag.PageTitle = "List of Recipes";
-      List<Recipe> model = _db.Recipes.ToList();
-      return View(model);
+      string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+
+      List<Recipe> userRecipes = _db.Recipes
+                                        .Where(entry => entry.User.Id == currentUser.Id)
+                                        // .Include(recipe.Category)
+                                        .ToList(); 
+      return View(userRecipes);
     }
-
-    // public async Task<ActionResult> Index()
-    // {
-    //   string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-    //   ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-
-    //   List<Recipe> userRecipes = _db.Recipes
-    //                                     .Where(entry => entry.User.Id == currentUser.Id)
-    //                                     // .Include(recipe.Category)
-    //                                     .ToList(); 
-    //   return View(userRecipes);
-    // }
     
-    // [HttpPost]
-    // public async Task<ActionResult> Create(Recipe recipe, int CategoryId)
-    // {
-    //   if (!ModelState.IsValid)
-    //   {
-    //     ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
-    //     return View(recipe);
-    //   }
-    //   else
-    //   {
-    //     string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-    //     ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-    //     recipe.User = currentUser;
-    //     _db.Recipes.Add(recipe);
-    //     _db.SaveChanges();
-    //     return RedirectToAction("Index");
-    //   }
-    // }
+    [HttpPost]
+    public async Task<ActionResult> Create(Recipe recipe, int CategoryId)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.PageTitle = "Add a new receta guey";
+        ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+        return View(recipe);
+      }
+      else
+      {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        recipe.User = currentUser;
+        _db.Recipes.Add(recipe);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
     public ActionResult Create()
     {
       ViewBag.PageTitle = "Add a new receta guey";
       return View();
     }
 
-    [HttpPost]
-    public ActionResult Create(Recipe recipe)
-    {
-      if (!ModelState.IsValid)
-      {
-        return View(recipe);
-      }
-      _db.Recipes.Add(recipe);
-      _db.SaveChanges();
-      return RedirectToAction("Index");
-    }
+    // [HttpPost]
+    // public ActionResult Create(Recipe recipe)
+    // {
+    //   if (!ModelState.IsValid)
+    //   {
+    //     return View(recipe);
+    //   }
+    //   _db.Recipes.Add(recipe);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // }
 
     public ActionResult Details(int id)
     {
